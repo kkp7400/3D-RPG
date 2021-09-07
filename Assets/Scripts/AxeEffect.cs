4555fac5f2655c13e9e0a202a7e2e262e564eea1
kkp7400 <53577948+kkp7400@users.noreply.github.com>
2021-09-07 15:50:48 +0900

    dd
    dd

--- Assets/Scripts/AxeEffect.cs
+++ Assets/Scripts/AxeEffect.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 public class AxeEffect : MonoBehaviour
 {
+    public delegate void StartDamageBox();
+    StartDamageBox startDamageBox;
     public EffectInfo[] Effects;
     public GameObject damageBox;
+    public GameObject damageSyl;
     [System.Serializable]
 
     public class EffectInfo
@@ -54,20 +57,41 @@ public class AxeEffect : MonoBehaviour
 
         yield break;
     }
+    //0 Strike
+    //1 Chop    
+    //3 Slash
     IEnumerator DamageBox(int skillNum)
     {
-        
         float afterTime = 3f;
-        Transform startPos = damageBox.transform;
-        damageBox.transform.position = this.transform.position;
-        damageBox.transform.rotation = this.transform.rotation;
-        while (afterTime >= 0)
+        if (skillNum == 0 || skillNum == 1)
         {
-            afterTime -= Time.deltaTime;
-            damageBox.transform.position += Vector3.forward * 0.5f;
-            yield return null;
+
+            Transform startPos = damageBox.transform;
+            damageBox.transform.position = this.transform.position;
+            damageBox.transform.rotation = this.transform.rotation;
+            while (afterTime >= 0)
+            {
+                afterTime -= Time.deltaTime;
+                damageBox.transform.position += damageBox.transform.forward * 1f;
+                yield return null;
+            }
+
+            damageBox.transform.position = startPos.position;
         }
-        damageBox.transform.position = startPos.position;
+        else if(skillNum == 3)
+        {
+            Transform startPos2 = damageSyl.transform;
+            damageSyl.transform.position = this.transform.position;
+            damageSyl.transform.rotation = this.transform.rotation;
+            while (damageSyl.transform.localScale.x <= 14f)
+            {
+                damageSyl.transform.localScale += new Vector3(0.6f, 0, 0.6f);
+                yield return null;
+            }
+
+            damageSyl.transform.position = new Vector3(1000,1000,1000);
+            damageSyl.transform.localScale = new Vector3(2,5,2);
+        }
     }
 
 }
